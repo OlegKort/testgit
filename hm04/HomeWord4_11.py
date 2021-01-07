cc3ef4dec90efafed90ed869d48c531e7e0881af
Oleg Kort <paskudnikby@gmail.com>
2021-01-07 14:19:05 +0300

    task 4_11 via for

--- hm04/HomeWord4_11.py
+++ hm04/HomeWord4_11.py
@@ -7,3 +7,12 @@ while length >= 0:
         sum += 1
     length -= 1
 print(sum)
+
+# Дан список целых чисел. Подсчитать сколько четных чисел в списке
+
+a = [2, 4, 5, 7, 8, 4]
+amount = 0
+for element in a:
+    if element % 2 == 0:
+        amount += 1
+print(amount)
