42121ce07bf748d905376c8b04c8c4d7a8191a9e
Oleg Kort <paskudnikby@gmail.com>
2021-01-07 15:57:25 +0300

    task 4_13 via for

--- hm04/Homeword4_13.py
+++ hm04/Homeword4_13.py
@@ -7,3 +7,13 @@ while b < len(a):
     b += 1
 new_list.append(a[0])
 print(new_list)
+
+# Дан список. Создать новый список, сдвинутый на 1 элемент влево Пример:12345 -> 23451
+a = [1, 2, 3, 4, 5, 6]
+new_list = []
+
+for i in range(len(a)):
+    if i != 0:
+        new_list.append(a[i])
+new_list.append(a[0])
+print(new_list)
