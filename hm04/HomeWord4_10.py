3ca900752580569e9b0a9665bf72ff9c2137f22d
Oleg Kort <paskudnikby@gmail.com>
2021-01-07 13:53:47 +0300

    task 4_10 via for

--- hm04/HomeWord4_10.py
+++ hm04/HomeWord4_10.py
@@ -8,3 +8,12 @@ while length >= 0:
 
 b.reverse()
 print(b)
+
+# Создать новый список, каждый элемент которого равен исходному элементу умноженному на -2
+array = [1, 5, 6, 8, 5, 7, 9]
+new_array = []
+
+for element in array:
+    new_array.append(element * -2)
+
+print(new_array)
